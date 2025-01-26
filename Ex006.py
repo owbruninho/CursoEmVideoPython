74993aba1e5cff530839875ddc8f2f12d55ab577
owbruninho <brunoschmidt13579@gmail.com>
2025-01-26 16:19:47 -0300

    Update Ex006.py

--- Ex006.py
+++ Ex006.py
@@ -2,5 +2,5 @@
 
 num = int(input('Digite um número: '))
 print('O dobro de {} vale {}.'.format(num, num * 2))
-print('O triplo de {} vale {}.'.format(num, num *3))
+print('O triplo de {} vale {}.'.format(num, num * 3))
 print('A raiz quadrada de {} vale {:.2f}.'.format(num, num ** (1/2)))
