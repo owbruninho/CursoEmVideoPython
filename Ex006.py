fbe6006016d92f5d727b7e488701d79b74bd838d
owbruninho <brunoschmidt13579@gmail.com>
2025-01-26 16:17:33 -0300

    Add files via upload

---
+++ Ex006.py
@@ -0,0 +1,6 @@
+# Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+
+num = int(input('Digite um número: '))
+print('O dobro de {} vale {}.'.format(num, num * 2))
+print('O triplo de {} vale {}.'.format(num, num *3))
+print('A raiz quadrada de {} vale {:.2f}.'.format(num, num ** (1/2)))
